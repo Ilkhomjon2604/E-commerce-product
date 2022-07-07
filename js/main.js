f261a1b2b04c698f6bfcf7e178011cd66a3d52b4
Ilkhomjon2604 <m347es@gmail.com>
2022-07-07 10:27:51 +0200

    adding advanced js to basket

--- js/main.js
+++ js/main.js
@@ -11,10 +11,23 @@ let elBasket = document.querySelector('.basket');
 let elCardContainer = document.querySelector('.card_container');
 let elNumberIndicator =  document.querySelector('.number_of_items');
 let elShoppingCardProduct =   document.querySelector('.shopping-card__wrapper');
+let elShoppingCardSelf = document.querySelector('.shopping-card-self');
+let elItemContainer = document.querySelector('.item_container');
 
+if(elBasket){
+  elBasket.addEventListener('click' , function(){
+    elCardContainer.classList.toggle('card_container__open')
+  });
+};
 
-elBasket.addEventListener('click' , function(){
-  elCardContainer.classList.toggle('card_container__open')
-});
+if(elShoppingCardProduct.childElementCount > 0){
+  elShoppingCardSelf.classList.add('shopping-card__wrapper-open');
+  elNumberIndicator.textContent = elShoppingCardProduct.childElementCount;
+}
+else{
+  elShoppingCardSelf.classList.remove('shopping-card__wrapper-open');
+  elItemContainer.classList.add('empty_item_container');
+  elNumberIndicator.textContent = elShoppingCardProduct.childElementCount;
+}
 
-console.log(elShoppingCardProduct);
+console.log(elShoppingCardProduct.childElementCount);
