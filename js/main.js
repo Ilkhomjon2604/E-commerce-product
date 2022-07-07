ecb8a4546fb62caa7ed2e5d7959135e9bab803ef
Ilkhomjon2604 <m347es@gmail.com>
2022-07-07 11:53:15 +0200

    change active class between imgs

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 // shopping-card__wrapper-open
 // btn-product__submit
 
+// card Product js
 
 let elBasket = document.querySelector('.basket');
 let elCardContainer = document.querySelector('.card_container');
@@ -13,6 +14,7 @@ let elNumberIndicator =  document.querySelector('.number_of_items');
 let elShoppingCardProduct =   document.querySelector('.shopping-card__wrapper');
 let elShoppingCardSelf = document.querySelector('.shopping-card-self');
 let elItemContainer = document.querySelector('.item_container');
+
 
 if(elBasket){
   elBasket.addEventListener('click' , function(){
@@ -28,6 +30,25 @@ else{
   elShoppingCardSelf.classList.remove('shopping-card__wrapper-open');
   elItemContainer.classList.add('empty_item_container');
   elNumberIndicator.textContent = elShoppingCardProduct.childElementCount;
-}
+};
 
-console.log(elShoppingCardProduct.childElementCount);
+// Img event js
+let elMainImage = document.querySelector('.main_img');
+let elsBunchOfImages = document.querySelectorAll('.bunch_of__snerakers');
+let elsbtnOfImages =  document.querySelectorAll('.small-imgs-wrapper-self');
+
+elsbtnOfImages.forEach( function(elButton){
+  elButton.addEventListener('click', function (){
+
+    // remove active class
+    elsbtnOfImages.forEach( function(elButton){
+      elButton.classList.remove('btn-acive-img')
+    });
+
+    // add active class to current element
+
+    elButton.classList.add('btn-acive-img')
+
+  });
+
+});
